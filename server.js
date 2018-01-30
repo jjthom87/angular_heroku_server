1f199ce65327efc8422af6550c86144d339e3740
Jared Thomas <jjthom87@yahoo.com>
2018-01-29 20:39:00 -0500

    adding

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+var models = require('./models');
+models.sequelize.sync();
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+app.use(forceSSL())
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use('/', require('./controller/routes.js'));
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
